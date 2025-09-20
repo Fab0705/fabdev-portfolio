c8e6be66fd406c39f0398597c19c63c4b115377e
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2025-09-19 22:59:11 -0500

    SVGR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+
 const nextConfig: NextConfig = {
-  /* config options here */
+   webpack: (config) => {
+    // Configuración para que SVGR funcione en Next.js
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/, // archivos JS/TS/JSX/TSX
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            // esto hace que exporte default como componente React
+            exportType: "default",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
